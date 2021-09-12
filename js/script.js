dcdea6d4ec596ca9d19cd873645499d4770829c3
DmitriiSlabozhanin <senya@vtomske.ru>
2021-09-12 23:08:11 +0700

    add lesson23

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable no-loop-func */
 /* eslint-disable no-unmodified-loop-condition */
 /* eslint-disable arrow-parens */
@@ -289,7 +290,99 @@ window.addEventListener('DOMContentLoaded', function(){
         portfolioDots.children[0].classList.add('dot-active');
     };
 
+    // Наводка на фотки
+
+    const setImg = () => {
+		const allCommand = document.querySelector('#command .row');
+
+		const changePhoto = () => {
+			const target = event.target;
+
+			if (target.classList.contains('command__photo')) {
+				const lastSrc = target.src;
+
+				target.src = target.dataset.img;
+				target.dataset.img = lastSrc;
+			}
+		};
+
+		allCommand.addEventListener('mouseover', changePhoto);
+		allCommand.addEventListener('mouseout', changePhoto);
+	};
+
+
+
+    // Ввод только цифр
+
+    const inputValidation = () => {
+
+    const calcBlock = document.querySelector('.calc-block');
+    const inputFields = document.querySelectorAll('input');
+
+    const checkCalc = () => {
+        calcBlock.addEventListener('input', (event) => {
+            if (event.target.type !== 'number') {
+                event.target.value = event.target.value.replace(/\D/g, '');
+            }
+        });
+    };
+
+    checkCalc();
+
+    const validateBlur = (elem) => {
+
+        let value = elem.value;
+        console.log('value: ', value);
+        console.log('elem: ', elem);
+
+        if (elem.name === 'user_message') {
+            value = value.replace(/((^[\s-]+))|((?<=\s)\s+)|((?<=-)-+)|([\s-]+$)/g, '');
+            elem.value = value;
+        } else if (elem.name === 'user_name') {
+            let words = value.match(/[а-я]+/ig);
+            words = words.map(item => (item.substring(0, 1).toUpperCase() + item.substring(1).toLowerCase()));
+            value = words.join(' ');
+            elem.value = value;
+        } else if (elem.name === 'user_email') {
+            value = value.replace(/(?<=@)@+/g, '');
+            elem.value = value;
+        } else if (elem.name === 'user_phone') {
+            value = value.replace(/((?<=\()\({1,})|((?<=\))\){1,})|((?<=-)-{1,})|((?<=\+)\+{1,})/g, '');
+            elem.value = value;
+        } else {
+            console.log('other input');
+        }
+    };
+
+    // Ввод Ваше имя, Ваше сообщение, e-mail
+    inputFields.forEach(elem => {
+        elem.addEventListener('input', (event) => {
+            const target = event.target;
+            let value = target.value;
+            if (elem.name === 'user_name' || elem.name === 'user_message') {
+                target.value = target.value.replace(/[^а-яё\s-]/ig, '');
+            } else if (elem.name === 'user_email') {
+                const regexpEmail = /([^a-z@_\-.!~*'])|((?<=^)@+)|((?<=@.*)@+)/ig;
+                target.value = value.replace(regexpEmail, '');
+            } else if (elem.name === 'user_phone') {
+                const regexpTelNumber = /([^\d()\-+])|((?<=.{35,}).)|((?<!^)\++)|((?<=^)-)|((?<=[+-])-+)|((?<=\([\d\-)(]*)\(+)|((?<=\()\)+)|((?<=\)[\d\-)(]*)\)+)|((?<=-)-+)/ig;
+                target.value = value.replace(regexpTelNumber, '');
+            }
+        });
+
+        elem.addEventListener('blur', (event) => {
+          const target = event.target;
+          console.log('blur at:', target);
+          validateBlur(target);
+        });
+    });
+
+    };
+
+
     dots();
+    setImg();
+    inputValidation();
     slider();
 
 });
